c87c80a6d485668f272e145bdf12a201fd132e41
Nicholas Karsant <nkarsant@gmail.com>
2022-04-13 13:34:46 -0700

    added search input field

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import {items, options} from './database.ts'
+import React, { useEffect, useState} from 'react';
+import {items, options} from './database.js'
 import {Accordion} from './Accordion'
+import {EnhancedTable} from './Datatable'
+import TextField from '@mui/material/TextField';
+
 
 export const App = () => {
 
+  const [searchValue, setSearchValue] = useState(null);
 
+
+
+
+  
 
 
   return (
+    <>
+      
     <div className="app">
       <Accordion items={items}/>
+      </div>
+
+    <div className='search'  style={{marginTop: '3em'}}>
+    <TextField sx={{ width:'50%' }}
+        onChange={(event) => {
+        if (typeof event.target.value === 'string') {
+          console.log(event.target.value)
+          setSearchValue(event.target.value);
+        }
+      }} label="Filter Data Table" />
+      </div>
+
+    <div className='table'>
+      <EnhancedTable filterTerm={searchValue}/>
     </div>
+    </>
   );
 }
 
